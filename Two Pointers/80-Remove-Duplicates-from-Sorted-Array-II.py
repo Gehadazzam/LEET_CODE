5c9b06e5bd05939610fe86b2276228fb10eb7351
Gehadazzam <101433042+Gehadazzam@users.noreply.github.com>
2024-07-24 14:37:05 +0300

    [80] [Time Beats: 74.73%] [Memory Beats: 83.94%] - LeetPush

---
+++ Two Pointers/80-Remove-Duplicates-from-Sorted-Array-II.py
@@ -0,0 +1,16 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        left = 0
+        count = 1
+        for right in range(1, len(nums)):
+            if nums[left] == nums[right]:
+                if count < 2:
+                    left += 1
+                    nums[left] = nums[right]
+                    count += 1
+            else:
+                left += 1
+                nums[left] = nums[right]
+                count = 1
+        
+        return left + 1
